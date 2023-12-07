cb46ce11955068dd3cfda71e1d66c15571c19ab2
Vyacheslav <guzxaroh@gmail.com>
2023-12-07 13:00:51 +0700

    Repository Info

---
+++ githubRepo/RepositoryInfo/RepositoryDetailInfoViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RepositoryDetailInfoViewController.swift
+//  githubRepo
+//
+//  Created by Vyacheslav on 01.12.2023.
+//
+
+import UIKit
+
+class RepositoryDetailInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        navigationItem.leftBarButtonItem?.title = ""
+//        navigationController?.navigationItem.leftBarButtonItem?.
+//        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+}
