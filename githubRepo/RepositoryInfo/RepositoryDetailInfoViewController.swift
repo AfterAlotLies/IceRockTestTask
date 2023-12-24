5f8994a325d938505f0ce9ad2a2acd0cdbb0af18
Vyacheslav <guzxaroh@gmail.com>
2023-12-24 20:19:57 +0700

    added display readme info

--- githubRepo/RepositoryInfo/RepositoryDetailInfoViewController.swift
+++ githubRepo/RepositoryInfo/RepositoryDetailInfoViewController.swift
@@ -6,26 +6,53 @@
 //
 
 import UIKit
+import Down
 
 class RepositoryDetailInfoViewController: UIViewController {
-
+    
     @IBOutlet private weak var topRepoInfoView: RepositoryDetail!
-
+    @IBOutlet private weak var readmeTextView: UITextView!
+    
     private var chosenRepoId: String = ""
+    
+    var readmeString: String = ""
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getRepositoryDetail()
+        
+        AppRepository.shared.getRepositoryReadme(ownerName: "", repositoryName: "", branchName: "") { repoReadme, error in
+            if error != nil {
+                print("bad(")
+            } else {
+                guard let repoInfo = repoReadme else { return }
+                DispatchQueue.main.async {
+                    self.renderMarkdown(markdown: repoInfo)
+                }
+            }
+        }
+    }
+    
+    func renderMarkdown(markdown: String) {
+        do {
+            let downView = try DownView(frame: self.readmeTextView.bounds, markdownString: markdown)
+            downView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+            self.readmeTextView.addSubview(downView)
+        } catch {
+            print("Error rendering Markdown: \(error.localizedDescription)")
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        getRepositoryDetail()
     }
     
     private func getRepositoryDetail() {
         AppRepository.shared.getRepository(repoId: chosenRepoId) { response, error in
             if error != nil {
-                print("smth bad there")
+                print(error?.localizedDescription ?? "error")
             } else {
                 guard let repoDetail = response else { return }
                 self.setInRepoDetail(repoDetail: repoDetail)
