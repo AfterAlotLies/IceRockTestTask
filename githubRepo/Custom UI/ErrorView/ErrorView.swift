4cae68203ba0254c133013cbb55066505cb68a92
Vyacheslav <guzxaroh@gmail.com>
2024-01-04 18:55:57 +0700

    added ErrorView for display some errors/warinings

--- githubRepo/Custom UI/ErrorView/ErrorView.swift
+++ githubRepo/Custom UI/ErrorView/ErrorView.swift
@@ -5,4 +5,90 @@
 //  Created by Vyacheslav on 03.01.2024.
 //
 
-import Foundation
+import UIKit
+import Reachability
+
+class ErrorView: UIView {
+    
+    @IBOutlet private weak var errorImage: UIImageView!
+    @IBOutlet private weak var errorTitle: UILabel!
+    @IBOutlet private weak var errorMessage: UILabel!
+    @IBOutlet private weak var retryButton: CustomButtonClass!
+    
+    let internetConnection = InternetConnection.shared.internetConnection
+    private var previousView : ControllerType = .other
+    weak var delegate: ErrorViewDelegate?
+    
+    public enum ControllerType : String {
+        case authController = "authController"
+        case repoListBadConnection = "repoListBadConnection"
+        case repoListEmpty = "repoListEmpty"
+        case repoDetailBadConnection = "repoDetailBadConnection"
+        case repoDetailReadmeError = "repoDetailReadmeError"
+        case other = "other"
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureView()
+    }
+    
+    private func configureView() {
+        let subview = self.loadViewFromXib()
+        subview.frame = self.bounds
+        subview.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.addSubview(subview)
+        setupButton()
+    }
+    
+    func setTypeOfPreviousView(type: ControllerType) {
+        previousView = type
+    }
+    
+    private func loadViewFromXib() -> UIView {
+        guard let view = Bundle.main.loadNibNamed("ErrorView", owner: self)?.first as? UIView else { return UIView() }
+        return view
+    }
+    
+    private func setupButton() {
+        retryButton.setButtonText(buttonText: "Retry")
+        retryButtonAction()
+    }
+    
+    public func hideErrorView() {
+        self.alpha = 0
+        errorImage.isHidden = true
+        errorTitle.isHidden = true
+        errorMessage.isHidden = true
+        retryButton.isHidden = true
+    }
+    
+    public func showErrorView() {
+        self.alpha = 1
+        errorImage.isHidden = false
+        errorTitle.isHidden = false
+        errorMessage.isHidden = false
+        retryButton.isHidden = false
+    }
+    
+    private func retryButtonAction() {
+        retryButton.actionHandler = {
+            try? self.internetConnection.startNotifier()
+            self.retryBackToAuthView()
+        }
+    }
+    
+    private func retryBackToAuthView() {
+        if internetConnection.connection == .wifi || internetConnection.connection == .cellular {
+            delegate?.retryAction()
+        } else {
+            internetConnection.stopNotifier()
+            showErrorView()
+        }
+    }
+}
