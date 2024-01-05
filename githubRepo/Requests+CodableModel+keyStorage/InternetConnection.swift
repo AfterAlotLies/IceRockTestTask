10c580364b3fbff2a3c8799cc2a46c12c23da591
Vyacheslav <guzxaroh@gmail.com>
2024-01-05 21:09:00 +0700

    new features

--- githubRepo/Requests+CodableModel+keyStorage/InternetConnection.swift
+++ githubRepo/Requests+CodableModel+keyStorage/InternetConnection.swift
@@ -9,7 +9,20 @@ import UIKit
 import Reachability
 
 final class InternetConnection {
+    
     static let shared = InternetConnection()
     private init() {}
+    
     public var internetConnection = try! Reachability()
+        
+    public func checkInternetConnection(successHandler: @escaping () -> Void, failureHandler: @escaping () -> Void) {
+        try? internetConnection.startNotifier()
+        if internetConnection.connection == .wifi || internetConnection.connection == .cellular {
+            internetConnection.stopNotifier()
+            successHandler()
+        } else {
+            internetConnection.stopNotifier()
+            failureHandler()
+        }
+    }
 }
